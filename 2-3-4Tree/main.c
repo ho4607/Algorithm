1708480321f1ec2b5d6855148f66e8cf5eddfa63
seong-ho-park <psh30095@gmail.com>
2021-11-16 16:55:15 +0900

    edit 234tree

--- 2-3-4Tree/main.c
+++ 2-3-4Tree/main.c
@@ -5,7 +5,7 @@
 
 typedef struct Node{
     int key[4];
-    int* parent;
+    Node *parent;
     Node *subNode[4];
 } Node;
 
@@ -22,30 +22,37 @@ int main (void){
 
 void insertion(int value, Node *N){
     // 현재 위치가 leaf node 일 때
-    if ( N->subNode[0] = NULL /*TODO: 언제 NIL인지 결정해야함.*/){
+    if ( (N->subNode[0] == NULL) && (N->subNode[1] == NULL) && (N->subNode[2] == NULL) && (N->subNode[3] == NULL) ){
 
-        if(N->key[0] = 0 || N->key[1] > value){ N->key[0] = value;} // 가장 왼쪽 키에 넣기
-        else if(N->key[1] = 0 || (N->key[0] < value && N->key[1] > value)){ N->key[1] = value;}
-        else if(N->key[2] = 0 || (N->key[1] < value && N->key[2] > value)){ N->key[2] = value;}
-        else if(N->key[3] = 0 || (N->key[2] < value && N->key[3] > value)){ 
+
+        if(N->key[0] == 0 ){ N->key[0] = value;}      // 첫전빼 키에 넣기
+        else if(N->key[1] == 0 ){ N->key[1] = value;} // 두번째 키에 넣기
+        else if(N->key[2] == 0 ){ N->key[2] = value;} // 세번째 키에 넣기
+        else if(N->key[3] == 0 ){                     // 네번째 키에 넣고 split 실행
             N->key[3] = value;
+            
+            //split 작업 시작
             insertion(N->key[2], N->parent);
+            N->parent->subNode[0] = &N;
+
             insertion(N->key[3], N->parent);
             N->key[2]=0;
             N->key[3]=0;
         }
     }
-    // 현재 위치가 core Node 일 때
-    else if(N->subNode[0] != NULL && (N->key[0] != NULL)){  
-        if(value <= N->key[0]){insertion(value, N->subNode[0]);}
-        else if(N->key[0] < value <= N->key[1]){insertion(value, N->subNode[1]);}
-        else if(N->key[1] < value <= N->key[2]){insertion(value, N->subNode[2]);}
-        else if(N->key[2] < value <= N->key[3]){insertion(value, N->subNode[3]);}
+    // 현재 위치가 core Node 일 때, 값이 같은 경우는 논하지 않음.
+    else if(N->subNode[0] != NULL && N->key[0] != 0){  
+        if(value < N->key[0]){insertion(value, N->subNode[0]);}
+        else if(N->key[0] < value && value < N->key[1]){insertion(value, N->subNode[1]);}
+        else if(N->key[1] < value && value < N->key[2]){insertion(value, N->subNode[2]);}
+        else if(N->key[2] < value && value < N->key[3]){insertion(value, N->subNode[3]);}
     }
-    // 현재 위치가 Core Node 이고, node에 element가 존재하지 않을 때 
-    else if( N->subNode[0] != NULL && (N-> key[0] = NULL)){
+    // 현재 위치가 Core Node 이고, node에 element가 존재하지 않을 때 / 새로운 노드값에 입력 
+    else if( N->subNode[0] != NULL && N-> key[0] == 0){
         N->key[0] = value;
+        //N->
     }
-    
-
 };
+
+// void split(Node *currentNode){
+// }
